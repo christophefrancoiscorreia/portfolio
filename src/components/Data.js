9927a4f1fe0b731b9798c2b046b1dd84678ab2d7
Christophe Correia <cc8design@gmail.com>
2020-10-23 21:42:30 +0200

    articles

--- src/components/Data.js
+++ src/components/Data.js
@@ -1,14 +1,33 @@
 export const postOne = {
+    img: '',
+    link: '',
+    gitLink: '',
+    title: '',
+    text: '',
+    alt: '',
+    self: false,
+};
+ 
+export const postTwo = {
+    img: '',
+    link: '',
+    gitLink: '',
+    title: '',
+    text: '',
+    alt: '',
+    self: false,
+};
+export const postThree = {
     img: 'images/pic02.jpg',
     link: '/budgety',
     gitLink: 'https://github.com/christophefrancoiscorreia/budget-income-manager',
     title: 'Budget manager',
-    text: 'Project in full vanila javascript.<br/>App to manage a monthly wallet.',
+    text: `Project in full vanila javascript.<br/>App to manage a monthly wallet.`,
     alt: 'Budget manager',
     self: true,
 };
-
-export const postTwo = {
+ 
+export const postFour = {
     img: 'images/pic03.jpg',
     link: '/forkify',
     gitLink: 'https://github.com/christophefrancoiscorreia/recipes-searcher',
@@ -18,27 +37,28 @@ export const postTwo = {
     self: true,
 };
 
-export const postThree = {
+export const postFive = {
     img: 'images/pic04.jpg',
     link: 'https://www.trainingneurosensoriel.com/',
     gitLink: '',
     title: '"Hipérion"<br/>Freelance Job',
-    text: 'Freelance fullstack development project.<br/>Extra: Webdesign and Logo design.<br>Wordpress for a psychotherapist office based in Paris.',
+    text: `Freelance fullstack development project.<br/>Extra: Webdesign and Logo design.<br>Wordpress for a psychotherapist office based in Paris.`,
     alt: '"Hipérion"<br/>Freelance Job',
     self: false,
 }; 
 
-export const postFour = {
+
+export const postSix = {
     img: 'images/pic05.jpg',
     link: 'https://hyperacousies.fr/',
     gitLink: '',
     title: '"hyperacousies"<br/>Freelance Job',
-    text: 'Freelance fullstack development project.<br>Extra: Webdesign and Logo design.<br>Wordpress CMS website for the same psychotherapist.',
+    text: `Freelance fullstack development project.<br/>Extra: Webdesign and Logo design.<br>Wordpress CMS website for the same psychotherapist.`,
     alt: '"hyperacousies"<br/>Freelance Job',
     self: false,
 }; 
 
-export const postFive = {
+export const postSeven = {
     img: 'images/pic05.jpg',
     link: 'https://hyperacousies.fr/',
     gitLink: '',
@@ -48,3 +68,31 @@ export const postFive = {
     self: false,
 }; 
 
+export const postEight = {
+    img: '',
+    link: '',
+    gitLink: '',
+    title: '',
+    text: '',
+    alt: '',
+    self: false,
+};
+export const postNine = {
+    img: '',
+    link: '',
+    gitLink: '',
+    title: '',
+    text: '',
+    alt: '',
+    self: false,
+};
+export const postTen = {
+    img: '',
+    link: '',
+    gitLink: '',
+    title: '',
+    text: '',
+    alt: '',
+    self: false,
+};
+ 
--- src/components/pages/HomePage/posts/PostArticle.js
+++ src/components/pages/HomePage/posts/PostArticle.js
@@ -5,15 +5,17 @@ import {
   } from 'react-icons/fa';
 
 function PostArticle({img, link, self, gitLink, title, text, alt}) {
+    
+
+
     return (
         <>
             <article>
                 <header>
-                    <h2><Link to={link} target={ self ? 'self' : '_blank' } rel={ self ? '' : 'noopener' }>{title}</Link></h2>
+                    <h2><Link to={link} target={ self ? 'self' : '_blank' } rel={ self ? '' : 'noopener' } dangerouslySetInnerHTML={{ __html: title }} /></h2>
                 </header>
                 <Link to={link} target={ self ? 'self' : '_blank' } rel={ self ? '' : 'noopener' } className="image fit"><img src={img} alt={alt} /></Link>
-                <p>{text}</p>
-                
+                <p dangerouslySetInnerHTML={{ __html: text }} />                
                 <ul class="actions special">
                     <li><Link to={link} target={ self ? 'self' : '_blank' } rel={ self ? '' : 'noopener' } class="button">Check it out</Link></li>
                 </ul> 
